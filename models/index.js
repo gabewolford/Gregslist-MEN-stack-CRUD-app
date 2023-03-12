edb1e72c25cced1ead1361a6bf0f3338d269b80d
gabewolford <gabewolford@gmail.com>
2023-03-12 14:25:43 -0700

    connected express app to mongoDB

--- models/index.js
+++ models/index.js
@@ -1,1 +1,13 @@
 // Connects to the database and contains all exported models & seed data
+
+// Require the Mongoose package & your environment configuration
+require('dotenv').config()
+const mongoose = require('mongoose');
+const mongodbUri = process.env.MONGODBURI;
+
+// Immediately invoked async function that will wait for
+// Mongoose to connect to MongoDB Atlas
+(async function () {
+    await mongoose.connect(mongodbUri);
+    console.log('Mongoose is connected to', mongodbUri);
+})().catch(err => console.log('MongoDB connection error:\n' + err))
