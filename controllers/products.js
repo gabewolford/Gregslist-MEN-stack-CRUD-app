10f921e87c491f89dfb8d86e5c246d9abf3c9ccf
gabewolford <gabewolford@gmail.com>
2023-03-13 11:12:59 -0700

    new, create, and edit routes set up and tested

--- controllers/products.js
+++ controllers/products.js
@@ -25,6 +25,22 @@ router.get('/', function (req, res) {
         })
 })
 
+// New Route (GET/Read): This route renders a form for user to
+// fill out to POST (create) a new listing
+router.get('/new', function (req, res) {
+    res.send('You hit the new route! There will be a form here.')
+})
+
+// Create Route (POST/Create): This route receives the POST request sent from the new route,
+// and creates a new product document using form data, and redirects the user
+// to the new product's show page
+router.post('/', function (req, res) {
+    db.Product.create(req.body)
+        .then(product => {
+            res.json(product)
+        })
+})
+
 // Show Route (GET/Read): Will display an individual product document
 // using the URL parameter (document _id)
 router.get('/:id', function (req, res) {
@@ -37,6 +53,14 @@ router.get('/:id', function (req, res) {
         .catch(() => res.send('404 Error: Page Not Found'))
 })
 
+// Edit Route (GET/Read): This route renders a form that the user will use
+// to PUT (edit) properties of an exisiting product
+router.get('/:id/edit', function (req, res) {
+    db.Product.findById(req.params.id)
+        .then(product => {
+            res.send('This is where you will edit exisiting product ' + product._id)
+        })
+})
 
 
 // Export routes so they are accessible is `server.js`
